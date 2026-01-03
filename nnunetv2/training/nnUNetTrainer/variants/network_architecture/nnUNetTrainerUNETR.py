ff422a5a78c6e85cd9cced7778e7a5c91164200b
b09401064 <b09401064@g.ntu.edu.tw>
2026-01-03 00:47:53 -0500

    Add some custom nnUNetTrainer classes: clDiceLoss, MONAI architectures (UNet, UNETR, SinUNETR)

---
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerUNETR.py
@@ -0,0 +1,69 @@
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+import torch
+
+import torch.nn as nn
+from typing import List, Tuple, Union
+from monai.networks.nets import UNETR
+
+
+class nnUNetTrainerUNet(nnUNetTrainer):
+    def __init__(
+        self,
+        plans: dict,
+        configuration: str,
+        fold: int,
+        dataset_json: dict,
+        device: torch.device = torch.device("cuda"),
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.enable_deep_supervision = False
+
+    @staticmethod
+    def build_network_architecture(architecture_class_name: str,
+                                   arch_init_kwargs: dict,
+                                   arch_init_kwargs_req_import: Union[List[str], Tuple[str, ...]],
+                                   num_input_channels: int,
+                                   num_output_channels: int,
+                                   enable_deep_supervision: bool = True) -> nn.Module:
+        """
+        This is where you build the architecture according to the plans. There is no obligation to use
+        get_network_from_plans, this is just a utility we use for the nnU-Net default architectures. You can do what
+        you want. Even ignore the plans and just return something static (as long as it can process the requested
+        patch size)
+        but don't bug us with your bugs arising from fiddling with this :-P
+        This is the function that is called in inference as well! This is needed so that all network architecture
+        variants can be loaded at inference time (inference will use the same nnUNetTrainer that was used for
+        training, so if you change the network architecture during training by deriving a new trainer class then
+        inference will know about it).
+
+        If you need to know how many segmentation outputs your custom architecture needs to have, use the following snippet:
+        > label_manager = plans_manager.get_label_manager(dataset_json)
+        > label_manager.num_segmentation_heads
+        (why so complicated? -> We can have either classical training (classes) or regions. If we have regions,
+        the number of outputs is != the number of classes. Also there is the ignore label for which no output
+        should be generated. label_manager takes care of all that for you.)
+
+        """
+        return UNETR(
+            in_channels=num_input_channels,
+            out_channels=num_output_channels,
+            img_size=arch_init_kwargs['patch_size'],
+            spatial_dims=3,
+            feature_size=16, hidden_size=768, mlp_dim=3072, num_heads=12, 
+            pos_embed='conv', norm_name='instance'
+         )
+         
+    def set_deep_supervision_enabled(self, enabled: bool):
+        """
+        This function is specific for the default architecture in nnU-Net. If you change the architecture, there are
+        chances you need to change this as well!
+        """
+        # if self.is_ddp:
+        #     mod = self.network.module
+        # else:
+        #     mod = self.network
+        # if isinstance(mod, OptimizedModule):
+        #     mod = mod._orig_mod
+
+        # mod.decoder.deep_supervision = enabled
+        pass
